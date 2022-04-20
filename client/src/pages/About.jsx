fe2d4422eed4b1bf402febada56cb5c6d9b34dc5
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2022-04-21 00:03:59 +0100

    completed teams section on about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,20 +1,40 @@
 import React from "react";
+import person1 from "../assets/images/people/p1.jpg";
+import person2 from "../assets/images/people/p2.jpg";
+import person3 from "../assets/images/people/p3.jpg";
 
 function About() {
   return (
     <div>
       {/* Hero section */}
       <section className="heroSection">
-        <div className="max-w-6xl mx-auto my-3 py-10 bg-zinc-300 rounded-lg">
+        <div className="max-w-6xl mx-auto my-3 py-10 bg-zinc-100 rounded-lg">
           <div className="w-full text-center">
             <h2 className="text-2xl font-bold text-center text-orange-600">
               About Us
             </h2>
-            <div className="font-bold text-5xl my-7 text-zinc-700">We're a team of <br />creators & innovators</div>
-            <p className="font-light max-w-xl mx-auto">The only corporate payment management system designed for user satisfaction and comfort. We help startups grow by offering help payment services they would have spent more time doing</p>
-            <div className="actions my-6 gap-1 flex justify-center">
-              <a href="#" className="btn rounded-3xl bg-black hover:bg-orange-600 text-white py-2 px-4">Get Started</a>
-              <a href="#" className="btn rounded-3xl border border-black hover:bg-black hover:text-white py-2 px-6">Pricing</a>
+            <div className="font-bold text-5xl my-7 text-zinc-700">
+              We're a team of <br />
+              creators & innovators
+            </div>
+            <p className="font-light max-w-xl mx-auto">
+              The only corporate payment management system designed for user
+              satisfaction and comfort. We help startups grow by offering help
+              payment services they would have spent more time doing
+            </p>
+            <div className="actions my-6 gap-2 flex justify-center">
+              <a
+                href="#"
+                className="btn rounded-3xl bg-black hover:bg-orange-600 text-white py-2 px-4"
+              >
+                Get Started
+              </a>
+              <a
+                href="#"
+                className="btn rounded-3xl border border-black hover:bg-black hover:text-white py-2 px-6"
+              >
+                Pricing
+              </a>
             </div>
           </div>
         </div>
@@ -23,7 +43,7 @@ function About() {
       {/* Values section */}
       <section className="valueSection">
         <div className="max-w-6xl mx-auto py-8">
-        <h3 className="text-center text-3xl mb-2">The Values We Live By</h3>
+          <h3 className="text-center text-3xl mb-2">The Values We Live By</h3>
           <p className="text-center font-light">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat ad
             asperiores repell.
@@ -81,6 +101,63 @@ function About() {
                 Learn more multiple payment options
               </a>
             </p>
+          </div>
+        </div>
+      </section>
+
+      {/* Team section */}
+      <section className="teamSection">
+        <div className="max-w-6xl mx-auto">
+          <h3 className="text-center text-3xl mb-2">Team</h3>
+          <p className="text-center font-light">
+            Meet The People Behind Boldwallet.
+          </p>
+          <div className="grid grid-cols-3 items-center gap-5 my-10">
+            <div className="grid-it">
+              <img
+                src={person1}
+                alt="person-1"
+                className="max-width-[250px] rounded"
+                style={{ objectFit: "cover" }}
+              />
+              <p className="font-bold text-2xl mt-3">Tomek</p>
+              <p className="font-zinc-5 text-zinc-600">Lead Frontend Developer</p>
+              <div className="icons flex gap-2 py-3 items-center text-xl text-zinc-600">
+                <a href="#"><i className="fa-brands fa-twitter hover:text-black"></i></a>
+                <a href="#"><i className="fa-brands fa-facebook-f hover:text-black"></i></a>
+                <a href="#"><i className="fa-brands fa-linkedin-in hover:text-black"></i></a>
+              </div>
+            </div>
+            <div className="grid-it">
+              <img
+                src={person2}
+                alt="person-1"
+                className="max-width-[250px] rounded"
+                style={{ objectFit: "cover" }}
+              />
+              <p className="font-bold text-2xl mt-3">Tomek</p>
+              <p className="font-zinc-5 text-zinc-600">Lead Frontend Developer</p>
+              <div className="icons flex gap-2 py-3 items-center text-xl text-zinc-600">
+                <a href="#"><i className="fa-brands fa-twitter hover:text-black"></i></a>
+                <a href="#"><i className="fa-brands fa-facebook-f hover:text-black"></i></a>
+                <a href="#"><i className="fa-brands fa-linkedin-in hover:text-black"></i></a>
+              </div>
+            </div>
+            <div className="grid-it">
+              <img
+                src={person3}
+                alt="person-1"
+                className="max-w-[380px] max-h-[300px] rounded"
+                style={{ objectFit: "cover" }}
+              />
+              <p className="font-bold text-2xl mt-3">Tomek</p>
+              <p className="font-zinc-5 text-zinc-600">Lead Frontend Developer</p>
+              <div className="icons flex gap-2 py-3 items-center text-xl text-zinc-600">
+                <a href="#"><i className="fa-brands fa-twitter hover:text-black"></i></a>
+                <a href="#"><i className="fa-brands fa-facebook-f hover:text-black"></i></a>
+                <a href="#"><i className="fa-brands fa-linkedin-in hover:text-black"></i></a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
