d7c2fcdda5c5e210fbf9c34cc9a080ebe5dcb71b
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2022-04-20 23:12:08 +0100

    Added values section in about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -19,6 +19,71 @@ function About() {
           </div>
         </div>
       </section>
+
+      {/* Values section */}
+      <section className="valueSection">
+        <div className="max-w-6xl mx-auto py-8">
+        <h3 className="text-center text-3xl mb-2">The Values We Live By</h3>
+          <p className="text-center font-light">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat ad
+            asperiores repell.
+          </p>
+        </div>
+        <div className="max-w-6xl mx-auto grid grid-cols-3 gap-3 py-12">
+          <div className="reason">
+            <p className="icon">
+              <span className="material-icons material-icons-outlined">
+                credit_card
+              </span>
+            </p>
+            <h3>Passion before money</h3>
+            <p className="detail font-thin text-sm my-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est vero
+              sed eum voluptate deserunt velit commodi obcaecati perferendis
+              assumenda! Ad, in asperiores!
+            </p>
+            <p>
+              <a href="#" className="text-blue-500 text-sm hover:underline">
+                Risk and Compliance
+              </a>
+            </p>
+          </div>
+          <div className="reason">
+            <p className="icon">
+              <span className="material-icons material-icons-outlined">
+                credit_card
+              </span>
+            </p>
+            <h3>Providing value</h3>
+            <p className="detail font-thin text-sm my-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est vero
+              sed eum voluptate deserunt velit commodi obcaecati perferendis
+              assumenda! Ad, in asperiores!
+            </p>
+            <p>
+              <a href="#" className="text-blue-500 text-sm hover:underline">
+                Grow with Boldwallet
+              </a>
+            </p>
+          </div>
+          <div className="reason">
+            <p className="icon">
+              <span className="material-icons">credit_card</span>
+            </p>
+            <h3>Teamwork</h3>
+            <p className="detail font-thin text-sm my-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est vero
+              sed eum voluptate deserunt velit commodi obcaecati perferendis
+              assumenda! Ad, in asperiores!
+            </p>
+            <p>
+              <a href="#" className="text-blue-500 text-sm hover:underline">
+                Learn more multiple payment options
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
